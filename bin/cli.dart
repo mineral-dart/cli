f92f8679e63724bc72261ef551d62f0bcec8d09f
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-10-06 02:28:48 +0200

    feat: implement make:command

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,9 +1,11 @@
+import 'package:cli/commands/make_command.dart';
 import 'package:cli/commands/make_event.dart';
 import 'package:mineral/api.dart';
 
 void main(List<String> arguments) async {
   final cli = Cli()
-    ..registerCommand(MakeEvent());
+    ..registerCommand(MakeEvent())
+    ..registerCommand(MakeCommand());
 
   await cli.handle(arguments);
 }
